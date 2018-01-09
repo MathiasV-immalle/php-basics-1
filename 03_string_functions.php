97ae04b7c6e336ce007a159e86fcf441961da033
MathiasV-immalle <mathias.vansant@immalle.be>
2018-01-09 15:19:39 +0100

    oef 3 af

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -1,11 +1,14 @@
 <?php
 
-$zin = "Binnenkort is het weeral lente";
+echo $zin = "Binnenkort is het weeral lente!";
 
 // Print af hoeveel woorden $zin bevat.
-
+$aantal = str_word_count($zin);
+echo "<p>De vorige zin bevat $aantal woorden!</p>";
 
 // Vervang in $zin "lente" door "zomer" en druk af.
-
+$newword = "zomer";
+$zin = str_replace("lente", $newword, $zin);
+echo "<p>$zin<p>"
 
 ?>

a227cd089da4316fa3785687337548a7a0091e5b
MathiasV-immalle <mathias.vansant@immalle.be>
2018-01-11 09:19:43 +0100

    oef 6 af

--- 06_arrays.php
+++ 06_arrays.php
@@ -1,17 +1,35 @@
 <?php
 
 // Maak een Array met 3 namen van medeleerlingen.
-
+$namen = array("jef","jan","jos");
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
 
+echo "<p>Met een for-loop itereren over een array:<br>";
+echo "(unordered list)</p>";
+echo "<ul>";
+for ($x = 0; $x < count($namen); $x++){
+	echo "<li>$namen[$x]</li>";
+}
+echo "</ul>";
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
 
-
-
-$naam = "Willy";
+echo "<p>Met een foreach-loop itereren over een array:<br>";
+echo "(ordered list)</p>";
+echo "<ol>";
+foreach($namen as $value){
+	echo "<li>".$value."</li>";
+}
+echo "</ol>";
 
 // Itereer met een for-loop over de variable $naam alsof het een array was.
+
+$naam = "Willy";
+echo "<ul>";
+for ($x = 0; $x < strlen($naam); $x++){
+	echo "<li>$naam[$x]</li>";
+}
+echo "</ul>";
 
 ?>

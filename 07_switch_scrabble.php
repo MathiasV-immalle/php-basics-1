e10a69c07ec03c68a3de6f9b0e416120d3c8d5a2
MathiasV-immalle <mathias.vansant@immalle.be>
2018-01-11 09:43:09 +0100

    oef 7 af

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -2,9 +2,40 @@
 
 $woord = "quotering";
 $punten = 0;
+$letters = str_split($woord, 1);
 
 // gebruik een switch-case-statement binnen een for-loop om
 // te itereren over een woord en de Scrabble-punten ervan te berekenen
+
+for ($x = 0; $x < strlen($woord); $x++){
+	switch ($letters[$x]) {
+	    case "q":
+		$punten += 10;
+		break;
+	    case "u":
+		$punten += 4;
+		break;
+	    case "o":
+		$punten += 1;
+		break;
+	    case "t":
+		$punten += 2;
+		break;
+	    case "e":
+		$punten += 1;
+		break;
+	    case "r":
+		$punten += 2;
+		break;
+	    case "i":
+		$punten += 1;
+		break;
+	    case "n":
+		$punten += 1;
+		break;
+	}
+}
+echo "<p>Wanneer men scrabble speelt zou men voor het woord \"$woord\" $punten punten krijgen.</p>";
 
 // Q 10 punten
 // U 4 punten
